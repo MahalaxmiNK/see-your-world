6f4c9758af6f2335be056ffbff56f3098e7363f8
MahalaxmiNK <mnk1632017@gmail.com>
2024-05-28 19:32:49 +0200

    complete feature implementation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 import styles from "./Button.module.css";
+
 function Button({ children, onClick, type }) {
   return (
-    <button onClick={onClick} className={`{$styles.btn} ${styles[type]}`}>
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
       {children}
     </button>
   );
